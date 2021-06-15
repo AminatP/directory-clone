4b9a373448c121b84a5d31832228b4fa73579c06
Aminat Puebla <ap2296@njit.edu>
2021-06-15 11:07:35 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -3,45 +3,58 @@ import React, { useState } from 'react'
 function App() {
   return (
     <div>
-      <Folder name="Documents">
-        <File name="resume.pdf"/>
-        <File name="coverLetter.pdf"/>
+      <Folder name="Desktop">
+        <Folder name="Documents">
+          <File name="resume.pdf"/>
+          <File name="coverLetter.pdf"/>
+        </Folder>
+        <Folder name="Music">
+          <File name="aria.mp4"/>
+          <File name="ListenToYourHeart.mp4"/>
+        </Folder>
+        <Folder name="Downloads">
+          <File name="Book.epub"/>
+          <File name="Book3.epub"/>
+        </Folder>
       </Folder>
-      <Folder name="Music">
-        <File name="aria.mp3"/>
-        <File name="ListenToYourHeart.mp3"/>
-      </Folder>
-      <Folder name="Downloads">
-        <File name="Book.epub"/>
-        <File name="Book3.epub"/>
-      </Folder>
+      <Folder name="Applications"></Folder>
     </div>
   );
 }
 
-
-
 const Folder = (props) => {
-  const [ value, setValue ] = useState(false)
+  const [ isOpen, setIsOpen ] = useState(false)
+  const { name, children } = props
+  const direction = isOpen ? 'down' : 'right'
 
-  const clickHandler = () => {
-    setValue(!value)
-  }
+  const handleClick = () => setIsOpen(!isOpen)
 
   return(
     <div>
-      <h3 onClick={clickHandler}>{props.name}</h3>
-      <p>{value ? props.children : null}</p>
+      <span onClick={handleClick}>
+        <i className="blue folder icon"></i>
+        <i className={`caret ${direction} icon`}></i>
+      </span>
+      {name}
+      <div style={{ marginLeft: '17px' }}>
+        {isOpen ? children : null}
+      </div>
     </div>
   )
 }
 
 const File = (props) => {
-  return(
-    <div>{props.name}</div>
-  )
-}
+  const { name } = props;
+  const fileExtension = name.split('.')[1];
+  const fileIcons = {
+    mp4: 'headphones',
+    pdf: 'file image',
+    epub: 'file image outline',
+  };
 
-
-
+  return <div>
+    <i className={`${fileIcons[fileExtension]} icon`}></i>
+    {name}
+  </div>
+};
 export default App;
