7145eec00c4c4a51c52b49422c3113510902e60d
Aminat Puebla <ap2296@njit.edu>
2021-06-15 10:36:08 -0400

    folder updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Folder name="Documents">
+        <File name="resume.pdf"/>
+        <File name="coverLetter.pdf"/>
+      </Folder>
+      <Folder name="Music">
+        <File name="aria.mp3"/>
+        <File name="ListenToYourHeart.mp3"/>
+      </Folder>
+      <Folder name="Downloads">
+        <File name="Book.epub"/>
+        <File name="Book3.epub"/>
+      </Folder>
     </div>
   );
 }
+
+
+
+const Folder = (props) => {
+  const [ value, setValue ] = useState(false)
+
+  const clickHandler = () => {
+    setValue(!value)
+  }
+
+  return(
+    <div>
+      <h3 onClick={clickHandler}>{props.name}</h3>
+      <p>{value ? props.children : null}</p>
+    </div>
+  )
+}
+
+const File = (props) => {
+  return(
+    <div>{props.name}</div>
+  )
+}
+
+
 
 export default App;
